777a39db992b93fcf7ed4d858a95f8fb1897eff9
MarcoB <mrc.bsllt@gmail.com>
2020-12-07 18:21:31 +0100

    primo commit test

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Con Vue, stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un’immagine presa anch’essa da un data.
+var app = new Vue(
+ {
+   el: '#main_title',
+   data: {
+     title: "Hello Word!"
+   },
+ }
+);
